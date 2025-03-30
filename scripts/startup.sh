18609b30371b0ca9ee6406671bf72a927f9ec277
Jair Anguiano <jairanpo@gmail.com>
2025-03-30 16:40:32 -0600

    feat: add hyprland and waybar dotfiles

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Get root privileges
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root."
-    exit 1
-fi
-
-xrandr --output HDMI-1 --scale .8x.8
 xbindkeys
 
 sudo virsh pool-destroy default 2> /dev/null
@@ -16,23 +9,3 @@ sudo virsh pool-create-as --name default --type dir --target ~/virtual_images
 
 rescuetime &
 blueman-manager &
-redshift -l "21.12908:-101.67374" &
-
-
-# List of websites to block
-BLOCKED_SITES=(
-    "www.youtube.com"
-)
-
-
-# Loop through blocked sites and add them to hosts file
-for site in "${BLOCKED_SITES[@]}"; do
-    if grep -q $site /etc/hosts; then
-        echo "$site is already blocked."
-    else
-        echo "Blocking $site..."
-        echo "127.0.0.1 $site" >> /etc/hosts
-    fi
-done
-
-echo "All sites have been blocked."

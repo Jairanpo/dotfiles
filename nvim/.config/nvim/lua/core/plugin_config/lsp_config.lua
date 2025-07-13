0100fbf8a7cdba9e3dbd247b76fe30de7a55345a
Jair Anguiano <jairanpo@gmail.com>
2025-07-13 00:33:00 -0600

    feat: add rust analyzer

--- nvim/.config/nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/.config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -40,6 +40,11 @@ end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+require("lspconfig").rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities
+})
+
 require("lspconfig").lua_ls.setup({
   on_attach = on_attach,
   capabilities = capabilities

64159b1f49b157d1f69991bf72128457a793cdb1
Jair Anguiano <jairanpo@gmail.com>
2025-09-25 19:18:09 -0600

    hyperland left-hand

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,7 +2,7 @@
 
 xbindkeys
 
-xrandr --output HDMI-1 --scale 0.5x0.5
+xrandr --output HDMI-A-1 --scale 0.5x0.5
 
 sudo virsh pool-destroy default 2> /dev/null
 sudo virsh pool-undefine default 2> /dev/null

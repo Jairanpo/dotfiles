1de3e32cfe432029faab23a2bc1f2212bcbc83ca
Jair Anguiano <jairanpo@gmail.com>
2025-11-11 20:47:30 -0600

    Use Rofi for i3

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,7 +2,7 @@
 
 xbindkeys
 
-xrandr --output HDMI-A-1 --scale 0.5x0.5
+xrandr --output HDMI-1 --scale 0.5x0.5
 
 sudo virsh pool-destroy default 2> /dev/null
 sudo virsh pool-undefine default 2> /dev/null
@@ -10,3 +10,4 @@ sudo virsh pool-undefine default 2> /dev/null
 sudo virsh pool-create-as --name default --type dir --target ~/virtual_images
 
 blueman-manager &
+i3-msg reload &

c2e2ca407ebc37a2435d318b97e5ce32f958e44b
Jair Anguiano <jairanpo@gmail.com>
2026-01-02 17:28:04 -0600

    Update startup script to start libvirt network

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -9,5 +9,10 @@ sudo virsh pool-undefine default 2> /dev/null
 
 sudo virsh pool-create-as --name default --type dir --target ~/virtual_images
 
+# Define and start libvirt networks for Vagrant
+sudo virsh net-define /etc/libvirt/qemu/networks/vagrant-libvirt.xml 2> /dev/null
+sudo virsh net-start vagrant-libvirt 2> /dev/null
+sudo virsh net-autostart vagrant-libvirt 2> /dev/null
+
 blueman-manager &
 i3-msg reload &

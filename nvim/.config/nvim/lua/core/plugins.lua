f02c204ab8948c95805708d7b31710ad83a5645a
Jair Anguiano <jairanpo@gmail.com>
2026-01-13 22:02:02 +0000

    Fix treesitter version

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -26,7 +26,7 @@ local plugins = {
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
   { "nvim-tree/nvim-web-devicons", opts = {} },
 	{'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' }},
-  {'nvim-telescope/telescope.nvim', version = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
+  {'nvim-telescope/telescope.nvim', version = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
   "github/copilot.vim",
   {
   "mason-org/mason.nvim",

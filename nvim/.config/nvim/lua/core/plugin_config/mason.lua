586e8dbea254d0238d6ecadc6f94df05fcaba349
Jair Anguiano <jairanpo@gmail.com>
2026-01-18 17:01:34 -0600

    Reset mason config

--- nvim/.config/nvim/lua/core/plugin_config/mason.lua
+++ nvim/.config/nvim/lua/core/plugin_config/mason.lua
@@ -1,81 +1,10 @@
-local mason = require("mason")
-local mason_lspconfig = require("mason-lspconfig")
-local mason_null_ls = require("mason-null-ls")
-
--- Setup Mason
-mason.setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
-    }
+-- run :h mason-introduction
+require("mason").setup({
+  -- we can add more registries where 
+  -- mason will find packages the order
+  -- matters, first registries added 
+  -- will have precedence:
+  registries = {
+    "github:mason-org/mason-registry"
   }
-})
-
--- Automatically install LSP servers
-mason_lspconfig.setup({
-  ensure_installed = {
-    "tsserver",           -- TypeScript language server (compatible name)
-    "lua_ls",            -- Lua language server (for Neovim config)
-    "html",              -- HTML language server
-    "cssls",             -- CSS language server
-    "jsonls",            -- JSON language server
-  },
-  handlers = {
-    -- Default handler for all servers
-    function(server_name)
-      require("lspconfig")[server_name].setup({
-        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-        on_attach = function(client, bufnr)
-          -- Basic keymaps
-          local opts = { noremap = true, silent = true, buffer = bufnr }
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        end
-      })
-    end,
-    
-    -- Custom handler for tsserver (TypeScript)
-    ["tsserver"] = function()
-      require("lspconfig").tsserver.setup({
-        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-        on_attach = function(client, bufnr)
-          -- Basic keymaps
-          local opts = { noremap = true, silent = true, buffer = bufnr }
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        end,
-        filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
-        settings = {
-          typescript = {
-            preferences = {
-              includeCompletionsForModuleExports = true,
-              includeCompletionsWithInsertText = true,
-            }
-          },
-          javascript = {
-            preferences = {
-              includeCompletionsForModuleExports = true,
-              includeCompletionsWithInsertText = true,
-            }
-          }
-        }
-      })
-    end
-  }
-})
-
--- Setup null-ls tools
-mason_null_ls.setup({
-  ensure_installed = {
-    "prettier",           -- Prettier formatter
-    "eslint_d",           -- ESLint daemon (faster ESLint)
-  },
 })

cb0bda3cf021564b3bc3d2a47b5ec1d6a3a51762
Jair Anguiano <jairanpo@gmail.com>
2026-01-19 19:29:35 -0600

    add workspace functions

---
+++ bin/.local/bin/lib/workspace.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Define the base directory for all projects
+# VAGRANT: /home/vagrant/workspace/repos/project-name/here
+
+get_work_directories(){
+  PATHS=(
+    "$HOME/workspace/repos"
+  )
+
+  # OS: /home/jair/workspace/client-name/workspace/repos/project-name/here
+  for deep_root in "$HOME/workspace"/*/workspace/repos; do
+      if [ -d "$deep_root" ]; then
+          PATHS+=("$deep_root")
+      fi
+  done
+
+  SEARCH_PATTERNS=(
+    "*/main"
+    "*/master"
+    "*/develop"
+    "*/feature/*"
+    "*/feat/*"
+    "*/fix/*"
+  )
+
+  args=("(")
+  first=true
+  for pattern in "${SEARCH_PATTERNS[@]}"; do
+    if [ "$first" = true ]; then
+      first=false
+    else
+      args+=("-o")
+    fi
+    args+=("-path")
+    args+=("$pattern")
+  done
+
+  args+=( \) )
+
+  echo $(find "${PATHS[@]}" -maxdepth 3 -type d "${args[@]}" 2> /dev/null)
+}
